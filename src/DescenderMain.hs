573921d63d358e6fb1813aebfb4b0d5acc3dc3cc
Andrew Cowie <andrew@operationaldynamics.com>
2020-02-19 17:01:20 +1100

    Implement problem 1, list files

--- src/DescenderMain.hs
+++ src/DescenderMain.hs
@@ -3,8 +3,20 @@
 
 module Main where
 
+import Control.Monad (forM_)
 import Core.Program
+import Core.System
+import System.Directory (listDirectory)
 
 main :: IO ()
-main = execute $ do
+main = execute program1
+
+program0 :: Program None ()
+program0 = do
     write "Hello World"
+
+program1 :: Program None ()
+program1 = do
+    files <- liftIO (listDirectory ".")
+    forM_ files $ \file -> do
+        writeS file
