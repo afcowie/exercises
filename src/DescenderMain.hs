ed840e82b6a0180e28a12934315e9233e2e3912b
Andrew Cowie <andrew@operationaldynamics.com>
2020-02-19 21:35:34 +1100

    Implement problem 2, list files recursively

--- src/DescenderMain.hs
+++ src/DescenderMain.hs
@@ -6,10 +6,10 @@ module Main where
 import Control.Monad (forM_)
 import Core.Program
 import Core.System
-import System.Directory (listDirectory)
+import System.Directory (listDirectory, doesDirectoryExist)
 
 main :: IO ()
-main = execute program1
+main = execute program2
 
 program0 :: Program None ()
 program0 = do
@@ -20,3 +20,20 @@ program1 = do
     files <- liftIO (listDirectory ".")
     forM_ files $ \file -> do
         writeS file
+
+program2 :: Program None ()
+program2 = descend "."
+  where
+    descend start = do
+        -- list the files in the given directory
+        files <- liftIO (listDirectory start)
+
+        -- loop over each file. If it is a directory, descend recursively.
+        -- Otherwise, print the path.
+        forM_ files $ \file -> do
+            let path = start ++ "/" ++ file
+            probe <- liftIO (doesDirectoryExist path)
+            if probe
+                then descend path
+                else writeS path
+
